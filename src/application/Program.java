291b7585aab8caa66cad698c86737ae46b43c04f
unknown <Wesleyscorrea@hotmail.com>
2022-08-12 11:25:20 -0300

    Update implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -38,6 +38,12 @@ public class Program {
         sellerDao.insert(newSeller);
         System.out.println("Inserted! new Id= " + newSeller.getId());
 
+
+        System.out.println("\n=== TESTE 5: sellerUpdate===");
+        seller = sellerDao.findById(1);
+        seller.setName("Martha Waine");
+        sellerDao.update(seller);
+        System.out.println("Update completed");
     }
 
 }
